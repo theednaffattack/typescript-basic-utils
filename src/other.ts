60b0ad7e14e7dba31aaef3e3577260a58ecd4543
Eddie Naff <eddie@eddienaff.dev>
2022-06-01 15:26:22 -0700

    Add distance utility

--- src/other.ts
+++ src/other.ts
@@ -31,3 +31,19 @@ export function randomIntFromInteger(min: number, max: number) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// https://github.com/christopher4lis/canvas-boilerplate/blob/master/src/js/utils.js
+export function randomIntFromRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function randomColor(colors: string[]) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+export function distance(x1: number, y1: number, x2: number, y2: number) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
